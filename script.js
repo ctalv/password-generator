56c307c64b29752424f79b13691ce5e741a3c6d9
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-07 21:19:22 -0600

    missed a save

--- script.js
+++ script.js
@@ -4,11 +4,19 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() { 
+  // Call the function to ask user prompts
   var password = generatePassword(); 
+  
+  // Function that will ask the user prompts
   function generatePassword () {
+    
+    // prompt series
     var first = prompt("1");
+
+    
   }
+  // Get references to the #password element
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
