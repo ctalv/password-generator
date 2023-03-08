02c78c404dc051e55fdfc9d5356443de5fbece9c
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-07 18:06:51 -0600

    added a prompt and some comments

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); 
+  function generatePassword () {
+    var first = prompt("1");
+  }
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
