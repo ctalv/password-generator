c6b8326816e11815014306b08ec44894a84e9a62
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-10 17:40:57 -0600

    removed testing

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function writePassword() {
     // prompt series + criteria
     var lengthPrompt = prompt("Choose a length for the password, at least 8 and up to 128 characters.");
     // validate length
-    // lengthPrompt = "f"; // TEST
     console.log(lengthPrompt);
     while ((lengthPrompt < 8) || (lengthPrompt > 128)) {
       lengthPrompt = prompt("Please enter a length for the password, at least 8 and up to 128 characters.");
@@ -28,7 +27,6 @@ function writePassword() {
     var lowercase = prompt("Would you like lowercase characters? Please type yes or no.");
     lowercase = lowercase.toUpperCase();
     // validate input 
-    lowercase = "YES"; // TEST
     while ((lowercase !== "YES") && (lowercase !== "NO")) {
       lowercase = prompt("Please type yes or no.");
       lowercase = lowercase.toUpperCase();
@@ -38,7 +36,6 @@ function writePassword() {
     var uppercase = prompt("Would you like uppercase characters? Please type yes or no.");
     uppercase = uppercase.toUpperCase();
     // validate input 
-    uppercase = "YES"; // TEST
     while ((uppercase !== "YES") && (uppercase !== "NO")) {
       uppercase = prompt("Please type yes or no.");
       uppercase = uppercase.toUpperCase();
@@ -48,7 +45,6 @@ function writePassword() {
     var numeric = prompt("Would you like numeric characters? Please type yes or no.");
     numeric = numeric.toUpperCase();
     // validate input 
-    numeric = "YES"; // TEST
     while ((numeric !== "YES") && (numeric !== "NO")) {
       numeric = prompt("Please type yes or no.");
       numeric = numeric.toUpperCase();
@@ -58,7 +54,6 @@ function writePassword() {
     var special = prompt("Would you like special characters? Please type yes or no.");
     special = special.toUpperCase();
     // validate input 
-    special = "YES"; // TEST
     while ((special !== "YES") && (special !== "NO")) {
       special = prompt("Please type yes or no.");
       special = special.toUpperCase();
